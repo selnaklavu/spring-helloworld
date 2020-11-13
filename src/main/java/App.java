4c6c445fc5c355be590b677fb8f114fe57a87b7c
selnaklavu <selnaklavu@gmail.com>
2020-11-13 16:45:12 +0800

    12

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,12 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld helloWorld1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld helloWorld2 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(helloWorld1 == helloWorld2);
+        System.out.println(cat1 == cat2);
     }
 }
